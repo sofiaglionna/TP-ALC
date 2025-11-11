083c53266ebed5dba329b6452d558eb9d0000608
felixS7777 <felix99cm@gmail.com>
2025-11-11 12:25:35 -0300

    Update modulo3.py

--- Modulos/modulo3.py
+++ Modulos/modulo3.py
@@ -63,7 +63,7 @@ def normaExacta(A,p):
                 max_norminf = suma
         res.append(max_norminf)
     # Caso norma 1 de A, tengo que buscar la maxima suma de los |elementos| por columna
-    if (1 in p) or (p == 1):
+    if (p != 'inf' and 1 in p) or (p == 1):
         #transpongo la matriz para poder usar el codigo anterior.
         matriz = traspuesta(A)
         max_norm1 = 0
