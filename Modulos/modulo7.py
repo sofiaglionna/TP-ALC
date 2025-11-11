ab9a7ab23f877661eb591403288c4c67b008cd9e
Pepe2704 <165918673+Pepe2704@users.noreply.github.com>
2025-11-11 00:15:14 -0300

    Update modulo7.py

--- Modulos/modulo7.py
+++ Modulos/modulo7.py
@@ -1,5 +1,71 @@
 import numpy as np
 
+
+# Funciones Auxiliares:
+
+def diagRH(A,tol=1e-15,K=1000):         ############################ FALTA MODIFICAR diagRH EN DONDE SE "@" POR LA FUNCION "multiplicacion_De_matrices_sin_numpy" ############################
+    if esSimetrica(A) == False:
+        return None
+        
+    v1, lambda1, _, _ = metpot2k(A, tol, K)  # v1 = primer autovector de A ; lambda1 = autovalor
+    
+    n = A.shape[0]
+    e1 = np.zeros(n)
+    e1[0] = 1  # e1 es el primer vector canonico
+
+    u = e1 - v1
+    Hv1 = np.eye(n) - 2 * (np.outer(u, u) / np.dot(u,u)) # np.outer(n, n) es producto externo ; np.dot(u,u) es la norma al cuadrado de (e1 - v1)
+
+    if n == 2:
+        S = Hv1
+        D = Hv1 @ A @ Hv1.T
+    else:
+        B = Hv1 @ A @ Hv1.T
+        A_moño = B[1:, 1:]
+        S_moño, D_moño = diagRH(A_moño,tol=1e-15,K=1000)
+        
+        D = np.zeros((n, n))
+        D[0, 0] = lambda1
+        D[1:, 1:] = D_moño
+
+        auxiliar = np.zeros((n, n))
+        auxiliar[0, 0] = 1
+        auxiliar[1:, 1:] = S_moño
+        S = Hv1 @ auxiliar
+
+    return S, D
+
+
+def multiplicacion_de_matrices_sin_numpy(A,B):
+    n = A.shape[0] # filas de A
+    m = A.shape[1] # columnas de A
+    r = B.shape[0] # filas de B
+    s = B.shape[1] # columnas de B
+
+    if m == n:
+        res = np.zeros((n, s))
+
+        for i in range(0, n ,1):
+            for j in range(0, s, 1):
+                sumatoria = 0
+                t = 0
+                while t < m:
+                    sumatoria += A[i, t] * B[t, j]
+                    t += 1
+                res[i,j] = sumatoria
+        return res
+
+    else:
+        raise ValueError("Las dimensiones no son compatibles para la multiplicación de matrices.")
+
+
+
+
+
+
+
+# Funciones del Módulo
+
 def transiciones_al_azar_continuas(n):
     A = np.random.rand(n,n)  # crea una matriz A de n x n con elementos reales entre 0 y 1
 
@@ -37,4 +103,87 @@ def transicion_al_azar_uniforme(n,thres):
 
 
 
+def nucleo(A,tol):
+    # Primero calculamos A^t por A y lo llamamos B
+    B = multiplicacion_de_matrices_sin_numpy(A.T,A)
 
+    # Luego uso diagRH (diagonalizacion con Householder)
+    S, D = diagRH(B, tol, K=1000)
+
+    # Los autovalores de B (A^t por A) son:
+    autovalores = np.diag(D)
+
+    # Creo una lista vacias que se guardaran los vectores del Nucleo normalizados:
+    v_nucleo = []
+
+    for i in range(0, len(autovalores), 1):
+        if abs(autovalores[i]) < tol:
+            v = S[:,i]
+            sumatoria = 0
+            for j in range(0,len(v),1):
+                sumatoria += (v[j])**2
+            norma = np.sqrt(sumatoria)
+            v = v / norma
+            v_nucleo.append(v)
+
+    if len(v_nucleo) == 0:
+        return np.array([])
+    else:
+        # Devuelvo los vectores como columnas
+        return np.array(v_nucleo).T
+            
+    ## SEGUN EL CHAT ESTA BIEN CODEADO ##
+
+
+
+def crea_rala(listado, m_filas, n_columnas, tol = 1e-15):
+    filas = listado[0]
+    columnas = listado[1]
+    valores = listado[2]
+
+    A_dict_res = {}
+
+    for i in range(0, len(valores), 1):
+        if abs(valores[i]) < tol:
+            valores[i] = 0
+
+    for j in range(0, len(valores), 1):
+        if valores[j] == 0:
+            None
+        else:
+            A_dict_res[(filas[j],columnas[j])] = valores[j]
+
+    res = [A_dict_res, (m_filas, n_columnas)]
+
+    return res
+
+
+    ## SEGUN EL CHAT ESTA BIEN CODEADO ##
+
+
+
+def multiplica_rala_vector(A, v):
+    # A es una matriz rala representada como [diccionario, (m, n)].
+    A_dict_res, (m, n) = A
+    
+    w = np.zeros(m)  # w = [0, 0,...,0] m (cantidad de filas de A) veces 
+
+    for clave in A_dict_res:        # "clave" es la tupla de las claves (fila, columna).
+        fila = clave[0]             # primer elemento de la tupla en las claves del diccionario.
+        columna = clave[1]          # segundo elemento de la tupla en las claves del diccionario.
+        valor = A_dict_res[clave]   # valor en esa posición.
+
+        w[fila] += valor * v[columna]   # la fila de w es igual al valor de la clave por la columna del vector v
+
+    return w
+
+
+A = crea_rala(listado, 6, 4)  # ---> [{(0, 1): 10, (2, 3): 4, (5, 0): 7}, (6, 4)]   --->  np.array([0, 10, 0, 0],   # fila 0
+                              #                                                                    [0, 0, 0, 0],    # fila 1
+                              #                                                                    [0, 0, 0, 4],    # fila 2
+                              #                                                                    [0, 0, 0, 0],    # fila 3
+                              #                                                                    [0, 0, 0, 0],    # fila 4
+                              #                                                                    [7, 0, 0, 0])    # fila 5
+ 
+v = np.array([1,2,3,4])
+#  print(multiplica_rala_vector(A, v))  ------>  [20.  0. 16.  0.  0.  7.]
