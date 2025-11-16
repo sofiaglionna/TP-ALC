c5fe5282f343114bdda9d4a327cfdcf100be71c5
Sofia <sofiaglionna@gmail.com>
2025-11-16 19:38:49 -0300

    5 y 3 hechos, el resto a revisar
    me duelen los ojos
    
    Co-Authored-By: felixS7777 <215231661+felixS7777@users.noreply.github.com>
    Co-Authored-By: Pepe2704 <165918673+Pepe2704@users.noreply.github.com>
    Co-Authored-By: Geronimo Pacheco Parrondo <130320529+pachecog20@users.noreply.github.com>

--- alc.py
+++ alc.py
@@ -1,15 +1,12 @@
 import numpy as np
 import pandas as pd
-from Modulos import traspuestaConNumpy as traspuesta, inversa, multiplicacionMatricialConNumpy as multiplicacionMatricial, calculaQR
+from Modulos import traspuestaConNumpy as traspuesta, inversa, multiplicacionMatricialConNumpy as multiplicacionMatricial, calculaQR, matricesIguales, esSimetricaConTol, calculaCholesky, sustitucionHaciaAdelante, sustitucionHaciaAtras, svd_reducida
 
 # ========================================
 #%% 1. LECTURA DE DATOS
 # ========================================
 
-# Cada embedding es un vector de dimension 1535 x 1. Cada embedding representa una imagen, donde en las 1536 filas, tiene un valor numerico ###
-# donde cada número representa una propiedad abstracta que la red aprendió a detectar como por ejemplo bordes, textura, forma, color, etc. ####
-
-# Cada número lo extrajo previamente EfficientNet cuando procesó la imagen, y nosotros no sabemos que representa cada valor, pero juntos forman una "firma matemática" de la imagen.
+# Cada embedding es un vector de dimension 1535 x 1. Cada embedding representa una imagen que procesó EfficientNet.
 
 ### DEVUELVE: Xt, Xv, Yt, Yv
 ### Donde:
@@ -74,10 +71,29 @@ La función se denomina pinvEcuacionesNormales(L, Y).
 -La función recibe la matriz X de los embeddings de entrenamiento, ---> Esto no lo entiendo pq no se le pasa como parámetro "X"
 -L la matriz de Cholesky, y Y la matriz de targets de entrenamiento. 
 -La función devuelve W.
-"""
 
-def pinVEcuacionesNormales(L, Y):
-    return True
+X pertenece a (n = 1536 x p = 2000) --->  n < p
+Aplica caso b) Si rango(X) = p, n < p, entonces X+=Xt 
+"""
+A = multiplicacionMatricial(Xt, traspuesta(Xt))
+
+L = calculaCholesky(A) #tolerancia 0 funciona?
+# A = LL^T
+traspuestaX = traspuesta(Xt)
+
+def pinVEcuacionesNormales(X, L, Y):
+    # L^T Z^T = X
+    L_traspuesta = traspuesta(L)
+    Z_traspuesta = sustitucionHaciaAtras(L_traspuesta, X) 
+    # L V^T = Z^T
+    V_traspuesta = sustitucionHaciaAdelante(L, Z_traspuesta)
+    
+    # Finalmente resolvemos W = Y × V:
+    # Donde V = pseudo inversa de X
+    V = traspuesta(V_traspuesta)
+    W_cholesky = multiplicacionMatricial(Y, V)
+    
+    return W_cholesky
 
 
 # ========================================
@@ -88,9 +104,58 @@ La función se denomina pinvSVD(U, S, V, Y).
 -La función recibe la matriz X de los embeddings de entrenamiento,
 -las matrices U, S, V de la descomposición SVD, e Y la matriz de targets de entrenamiento. 
 -La función devuelve W.
+
+Parámetros:
+-----------
+U : np.array
+    Matriz U de la descomposición SVD (nxr) donde r = rango(X)
+S : np.array
+    Matriz diagonal Σ con valores singulares (rxr)
+V : np.array
+    Matriz V de la descomposición SVD (pxr)
+Y : np.array
+    Matriz de targets (mxp)
+
+- X = U·Σ·Vᵀ es la descomposición SVD
+- X⁺ = V·Σ⁺·Uᵀ es la pseudoinversa
+- W = Y·X⁺ = Y·V·Σ⁺·Uᵀ
 """
-def pinvSVD(U, S, V, Y):
-    return True
+# X pertenece a (n = 1536 x p = 2000) --->  n < p
+
+# Desc de X
+U, S, Vt = svd_reducida(Xt, k="max")
+
+def pinvSVD(U, S, Vt, Y):
+    
+    V = traspuesta(Vt) # svd devuelve V traspuesta asi que la trasponemos de nuevo para obtener V.
+
+    Ut = traspuesta(U)
+
+    #Pseudoinversa de sigma:
+    diag_S = np.diag(S)
+
+    n = Ut.shape[0] #1536
+    r = S.shape[0] 
+
+    diag_S_inv = np.zeros(r)
+    tol = 1e-10
+    
+    for i in range(r):
+        if abs(diag_S[i]) > tol:
+            diag_S_inv[i] = 1.0 / diag_S[i]
+        else:
+            diag_S_inv[i] = 0.0
+    
+    # Creamos la pseudoinversa de sigma como matriz diagonal
+    Sigma_pseudo = np.diag(diag_S_inv) #pxn
+
+    VxSigma = multiplicacionMatricial(V,Sigma_pseudo) # V = pxp , Sigmapseudo = pxn , ---> VxSigma = pxn 
+
+    VxSigmaxU = multiplicacionMatricial(VxSigma, Ut) # VxSigma = pxn , Ut = nxn , --->  VxSigmaxU = pxn
+
+    W_SVD = multiplicacionMatricial(Y, VxSigmaxU) # Y = mxp , VxSigmaxU = pxn , ---> W_SVD = mxn
+
+    return W_SVD
 
 # ========================================
 #%% 4. DESCOMPOSICIÓN QR - Algoritmo 3
@@ -118,9 +183,13 @@ QHH = QHH[0:filas,0:columnas]
 RHH = RHH[0:columnas,0:columnas]
 
 def pinvHouseHolder(Q, R, Y):
+    #trasponemos las 2 matrices para hacer (R^T)*(V^T) = (Q^T) y despejar V^T
     Rtraspuesta = traspuesta(R)
-    pseudoInversaX = multiplicacionMatricial(Q, inversa(Rtraspuesta))
-    W = multiplicacionMatricial(Y,pseudoInversaX)
+    Qtraspuesta = traspuesta(Q)
+    #resolvemos el sistema (R ya es triangular inferior)
+    Vtraspuesta= sustitucionHaciaAdelante (Rtraspuesta, Qtraspuesta)
+    V = traspuesta(Vtraspuesta)
+    W = multiplicacionMatricial(Y,V)
     return W
 
 #%% b) Gram-Schmidt 
@@ -145,7 +214,34 @@ RGS = RGS[0:columnas,0:columnas]
 #el enunciado dice gram schmidt clasico, nosotros usamos el modificado. Lo cambiamos?
 ##########################################
 def pinvGramSchmidt(Q, R, Y):
+    #trasponemos las 2 matrices para hacer (R^T)*(V^T) = (Q^T) y despejar V^T
     Rtraspuesta = traspuesta(R)
-    pseudoInversaX = multiplicacionMatricial(Q, inversa(Rtraspuesta))
-    W = multiplicacionMatricial(Y,pseudoInversaX)
+    Qtraspuesta = traspuesta(Q)
+    #resolvemos el sistema (R ya es triangular inferior)
+    Vtraspuesta= sustitucionHaciaAdelante (Rtraspuesta, Qtraspuesta)
+    V = traspuesta(Vtraspuesta)
+    W = multiplicacionMatricial(Y,V)
     return W
+
+# ========================================
+#%% 5. Pseudo-Inversa de Moore-Penrose 
+# ========================================
+
+"""
+Recibe dos matrices y devuelva True si verifican las condiciones de Moore-Penrose. 
+En caso contrario devolver False.
+"""
+
+def esPseudoInverda(X, pX, tol=1e-8):
+    #primera condicion: A*(A^+)*A = A
+    a = multiplicacionMatricial(multiplicacionMatricial(X, pX),X) 
+    #segunda condicion: (A^+)*A*(A^+) = (A^+)
+    b = multiplicacionMatricial(multiplicacionMatricial(pX, X), pX) 
+    #tercera condicion: (A*(A^+))^T = (A*(A^+)) (simetría)
+    c = traspuesta(multiplicacionMatricial(X,pX))
+    #cuarta condicion: ((A^+)*A)^T = ((A^+)A*) (simetría)
+    d = traspuesta(multiplicacionMatricial(pX,X))
+
+    if (not matricesIguales(X,a,tol = tol)) or (not matricesIguales(pX,b,tol = tol)) or not(esSimetricaConTol(c,tol = tol)) or not(esSimetricaConTol(d,tol = tol)):
+        return False
+    return True
